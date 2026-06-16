eca17fc7a440f9184a0e25800dd72331aa15e42a
Rocio Heredia <rocioheredia264@gmail.com>
2026-06-16 11:28:42 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/app_gestion_abogados/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_gestion_abogados
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_gestion_abogados/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
