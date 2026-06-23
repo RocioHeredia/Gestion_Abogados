719afdc6d129622698926c8691af54f962b9f5a4
Rocio Heredia <rocioheredia264@gmail.com>
2026-06-23 12:41:12 -0300

    Cambio en login_page

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -170,7 +170,7 @@ class _LoginPageState extends State<LoginPage> {
 
                               Navigator.pushReplacementNamed(
                                 context,
-                                '/Consultas',
+                                '/HomePages',
                               );
                             },
                             child: const Text(
